7725a18231d4d3fea589c56516e3ed46ac44c8e5
viron11111 <andygrayia@gmail.com>
2015-04-26 21:33:36 -0400

    more changes

--- Heart_beat_test/Heart_beat_test.ino
+++ Heart_beat_test/Heart_beat_test.ino
@@ -1,3 +1,15 @@
+#include "Timer.h"
+#include "fontsLCD.h"
+#include <SoftwareSerial.h>
+
+fontsLCD f;  //initialize font library
+int no_pulse_counter = 0;
+int beat_acquiring_counter = 0; //counter for adjusting screen saver
+int change_number_counter = 0;
+int heart_counter = 0;
+
+Timer t;  //initialize timer library
+
 int amount = 1000;
 unsigned int a_read = 0;
 float sum = 0.0;
@@ -7,17 +19,20 @@ int i = 0;
 long lastDebounceTime = 0;
 long debounceDelay = 400;
 
-long counter = 0;
-
 boolean no_pulse_latch = 1;
 boolean acq_pulse_latch = 0;
 boolean beat_pulse_latch = 0;
 boolean beat_latch = 0;
+byte state = B0;
+byte state_new;
+byte state_old = B1111;
 
 //Variables used to measure heart rate
 unsigned long oldtime = 0;
 unsigned long newtime = 0;
 int beat_time = 0;
+float BPM_new = 0.0;
+float BPM_old = 0.0;
 float BPM = 0.0;
 int num1_old = 10;
 int num2_old = 10;
@@ -30,109 +45,122 @@ int send2 = 10;
 int send3 = 10;
 
 void setup() {
+  //for acquiring hb screen
+  randomSeed(analogRead(1));
+  
   // initialize the serial communication:
-  Serial.begin(9600);
+  Serial.begin(115200);
+  
   pinMode(2, INPUT); // Setup for leads off detection LO +
   pinMode(3, INPUT); // Setup for leads off detection LO -
 
   attachInterrupt(0, right_Hand_Detect, FALLING);
   
-  i = 0;
+  lastDebounceTime = millis();
+  //t.every(100, display);
 }
 
 void loop() {
   
   if (digitalRead(2) == 0 && digitalRead(3) == 0 && (millis() - lastDebounceTime) > debounceDelay && no_pulse_latch == 1){  //***************************** Hands detected *****************************
-    Serial.print("detecting beat...");
-    Serial.println(millis());
-    no_pulse_latch = 0;
-    acq_pulse_latch = 1;
-    beat_pulse_latch = 0;    
-    beat_latch = 0;
-    
-    lastDebounceTime = millis();
-    detachInterrupt(0); 
-    //attachInterrupt(1, hands_Off, RISING);   
+    hands_detected();
   }
   else if (acq_pulse_latch == 1 && digitalRead(2) == 0 && digitalRead(3) == 0){
-    sum = 0.0;    
-    
-    for (i=0; i <= amount; i++){  
-      sum = sum + analogRead(A0);
-      delay(1);
-    }    
-    
-    average = sum/amount;
-    
-    Serial.print("Average value = ");
-    Serial.println(average);
-
-    while(analogRead(A0) <= average + 20){
-      delay(1);
-    }  
-    beat_time = newtime - oldtime;
-    beat_latch = 1;
-    
-    Serial.println("First Beat");    
-    
-    no_pulse_latch = 0;
-    acq_pulse_latch = 0;
-    beat_pulse_latch = 1;     
-    debounceDelay = 400;
-  }  
-  
+    acquiring();
+  }    
   else if (beat_pulse_latch == 1 && digitalRead(2) == 0 && digitalRead(3) == 0){
-    int difference = 0;
-    difference = a_read - average;
-    /*Serial.print("average: ");
-    Serial.print(average);
-    Serial.print(" a_read: ");
-    Serial.print(a_read);
-    Serial.print(" difference: ");
-    Serial.println(difference);
-    delay(100);*/
-
-    //Serial.println(difference);
-    if (beat_latch == 0 && a_read >= (average+25) && (millis() - lastDebounceTime) > debounceDelay){
-      newtime = millis();
-      beat_time = newtime - oldtime;
-      //Serial.print(difference);
-      //Serial.println(" Beat time");
-      beat_latch = 1;
-      lastDebounceTime = millis();
-      oldtime = newtime;
-      
-      BPM = 60 / (((float) beat_time) / 1000);
-      Serial.print("BPM: ");
-      Serial.println(BPM);
-    }
-    else if (beat_latch == 1 && a_read < average-20){
-      beat_latch = 0;
-      //Serial.println("cleared latch....");
-      
-    }
-    
-
-    
-  }  
-  
-  
-  
+    measure_beat();
+  }    
   else if (((digitalRead(2) == 1)||(digitalRead(3) == 1)) && (millis() - lastDebounceTime) > debounceDelay && (acq_pulse_latch == 1 || beat_pulse_latch == 1)){ //*************************** Hands Removed ***************************
-    Serial.print("Hands off device...");
-    Serial.println(millis());
-    no_pulse_latch = 1;
-    acq_pulse_latch = 0;
-    beat_pulse_latch = 0;
-    average = 0.0;
-    debounceDelay = 400;
-    attachInterrupt(0, right_Hand_Detect, FALLING);
+    no_hands();
   }
   
   a_read = analogRead(A0);
+  t.update();  //For updating timer used with diplaying graphic
   delay(1);
-
 }
+
+
+void hands_detected(){
+  if(digitalRead(2) == 0 && digitalRead(3) == 0){  //Added to prevent "no pulse dectected during transition from acq to detected
+    Serial.println("Hands detected...");
+    no_pulse_latch = 0;
+    acq_pulse_latch = 1;
+    beat_pulse_latch = 0;
+    state = B0;
+    num1_old = 10;
+    num2_old = 10;
+    num3_old = 10;
+    lastDebounceTime = millis();
+    detachInterrupt(0); 
+  }
+}
+
+void acquiring(){
+  
+  sum = 0.0;    
+    
+  for (i=0; i <= amount; i++){  
+    sum = sum + analogRead(A0);
+    delay(1);
+  }    
+  
+  average = sum/amount;
+  
+  Serial.print("Average value = ");
+  Serial.println(average);
+
+  while(analogRead(A0) <= average + 20){
+    if (digitalRead(2) == 1 || digitalRead(3) == 1){
+      break;
+    }
+    delay(1);
+  }  
+  beat_time = newtime - oldtime;
+  beat_latch = 1;
+  
+  Serial.println("First Beat");    
+  
+  no_pulse_latch = 0;
+  acq_pulse_latch = 0;
+  beat_pulse_latch = 1;     
+  debounceDelay = 400;
+
+  state = B10;
+  lastDebounceTime = millis();
+}
+
+void measure_beat(){
+  if (beat_latch == 0 && a_read >= (average+25) && (millis() - lastDebounceTime) > debounceDelay){
+    newtime = millis();
+    beat_time = newtime - oldtime;
+    beat_latch = 1;
+    lastDebounceTime = millis();
+    oldtime = newtime;
+    
+    BPM = 60 / (((float) beat_time) / 1000);
+    
+    if ((BPM < 200.0) && (BPM > 40)){
+      Serial.print("BPM: ");
+      Serial.println(BPM);
+    }
+  }
+  else if (beat_latch == 1 && a_read < average-20){
+    beat_latch = 0;
+  }
+}
+
+void no_hands(){
+  Serial.print("Hands off device...");
+  Serial.println(millis());
+  no_pulse_latch = 1;
+  acq_pulse_latch = 0;
+  beat_pulse_latch = 0;
+  average = 0.0;
+  debounceDelay = 400;
+  attachInterrupt(0, right_Hand_Detect, FALLING);
+}
+  
 
 void right_Hand_Detect(){
   if(digitalRead(3) == 0 && no_pulse_latch == 1){
